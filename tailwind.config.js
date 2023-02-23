6bbb4decd24088a571af029b175a4cbc05803cda
rifkysyahreza <rsyahrezafs@gmail.com>
2023-02-23 13:14:10 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#1565D8",
+        primary: "#00B9FF",
+        search: "#00B8D9",
+      },
+    },
+  },
+  plugins: [],
+};
